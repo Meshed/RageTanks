06736f4a4e23511c48d6bdda2db3d2449acc124b
Mark Brown <marksbrown@gmail.com>
2015-10-17 18:18:28 -0400

    Finished with the enemy. Add some to the platforms.

---
+++ Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelfDestruct : MonoBehaviour
+{
+    public float fuseLength = 0.1f;
+    private float destructTime = 0.0f;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    destructTime = Time.time + fuseLength;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    if (destructTime < Time.time)
+	    {
+	        Destroy(gameObject);
+	    }
+	}
+}
